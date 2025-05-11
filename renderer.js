c52117d0db508803a719a4a2f917c9dac15a2a9a
Code Monkey King <freesoft.for.people@gmail.com>
2025-05-11 12:12:06 +0300

    added showboard exception

--- renderer.js
+++ renderer.js
@@ -179,7 +179,8 @@ input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     const input = document.getElementById('input');
     window.katagoAPI.sendCommand(input.value);
-    if (input.value != 'final_score') window.katagoAPI.sendCommand('showboard');
+    if (input.value != 'final_score' && input.value != 'list_commands')
+      window.katagoAPI.sendCommand('showboard');
     input.value = '';
     let terminal = document.getElementById('output');
     terminal.scrollTop = terminal.scrollHeight;
