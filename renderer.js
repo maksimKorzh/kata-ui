309725a1d0b1eda2adbc601bd6aed4f146e044bf
Code Monkey King <freesoft.for.people@gmail.com>
2025-06-01 01:11:20 +0300

    made UI more responsive

--- renderer.js
+++ renderer.js
@@ -103,9 +103,9 @@ function resizeCanvas() {
   canvas.height = canvas.width;
   drawBoard();
   try {
-    document.getElementById('output').style.width = canvas.width-220 + 'px';
+    document.getElementById('output').style.width = (window.innerWidth - canvas.width-34)  + 'px';
     document.getElementById('output').style.height = canvas.width-44 + 'px';
-    document.getElementById('input').style.width = canvas.width-230 + 'px';
+    document.getElementById('input').style.width = (window.innerWidth - canvas.width-34)  + 'px';
   } catch (e) {}
 }
 
@@ -122,8 +122,8 @@ function resizeCanvas() {
   clearBoard();
   resizeCanvas();
   document.getElementById('terminal').innerHTML = `
-    <pre id="output" style="width: ` + (canvas.width-220) + `px; height: ` + (canvas.width-44) + `px; color: white; font-family: monospace; font-size: calc(100vw/113); overflow: auto;"></pre>
-    <input id="input" spellcheck="false" placeholder="Type a command..." style="width: ` + (canvas.width-230) + `px; font-size: 18px;" autofocus>
+    <pre id="output" style="width: ` + (window.innerWidth - canvas.width-34) + `px; height: ` + (canvas.width-44) + `px; color: white; font-family: monospace; font-size: calc(100vw/113); overflow: auto;"></pre>
+    <input id="input" spellcheck="false" placeholder="Type a command..." style="width: ` + (window.innerWidth - canvas.width-34) + `px; font-size: 18px;" autofocus>
   `;
 })();
 
